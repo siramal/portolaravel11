3f5d2fc3bb910e205d8e53f13c258834127e38b5
unknown <sirojulkamal13@gmail.com>
2024-06-13 19:37:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
